cecb72b362c51148661a68bfdb20366e641faf17
JRodriAlarc <Jona10131313@gmail,com>
2024-03-26 16:50:20 -0600

    Suir los archivos de mi Proyecto

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Obtener referencia a los elementos del DOM
+const contadorElemento = document.getElementById("contador");
+const botonIncrementar = document.querySelector("button");
+
+// Inicializar contador
+let contador = 0;
+
+// Función para actualizar el contador y mostrarlo en la página
+function actualizarContador() {
+  contador++;
+  contadorElemento.textContent = contador;
+}
+
+// Agregar un event listener al botón para que llame a la función cuando se hace clic
+botonIncrementar.addEventListener("click", actualizarContador);
